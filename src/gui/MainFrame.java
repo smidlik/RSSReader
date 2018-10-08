89da76be155f0b6eca3581014b70a71cf40309ea
smidlik <smidajakub@email.cz>
2018-10-08 13:26:53 +0200

    test

---
+++ src/gui/MainFrame.java
@@ -0,0 +1,16 @@
+package gui;
+
+import javax.swing.*;
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        init();
+    }
+
+
+    private void init() {
+     setTitle("RSS Reader");
+     setSize(800,600);
+     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+     setLocationRelativeTo(null);
+    }
+}
